02f63ab543139bf2d38ead8f605bbac00e68ccb1
semenovaoa90-source <semenovaoa90@gmail.com>
2026-07-06 06:26:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import os
+import re
+import requests
 
-print("BOT_TOKEN:", repr(os.getenv("BOT_TOKEN")))
-print("CHAT_ID:", repr(os.getenv("CHAT_ID")))
+URL = "https://prodoctorov.ru/nnovgorod/vrach/1032093-ivanova/"
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+r = requests.get(URL, headers=headers, timeout=30)
+html = r.text
+
+m = re.search(
+    r'"nearest_appointment_express_datetime":"([^"]+)"',
+    html
+)
+
+if m:
+    text = (
+        "Найдена ближайшая запись:\n"
+        + m.group(1)
+        + "\n\n"
+        + URL
+    )
+else:
+    text = "Поле nearest_appointment_express_datetime не найдено."
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": text
+    },
+    timeout=20,
+)
